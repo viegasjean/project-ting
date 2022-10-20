d99d07923c6175471029cbd2769d7df00a0b84d5
Jean Viegas <jeanmelg@gmail.com>
2022-10-20 19:21:06 -0300

    feat: :sparkles: search_by_word function returning text content if word match

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -18,4 +18,19 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    lista = instance.search(0)['linhas_do_arquivo']
+    arquivo = instance.search(0)['nome_do_arquivo']
+
+    ocorrencias = [
+        {'linha': i + 1, 'conteudo': text}
+        for i, text
+        in enumerate(lista)
+        if word.lower()
+        in text.lower()]
+    if ocorrencias:
+        return [{
+            'arquivo': arquivo,
+            'palavra': word,
+            'ocorrencias': ocorrencias
+        }]
+    return []
