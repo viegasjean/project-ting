1d66ec3d9d0b055b466a771729ae8637685eda8c
Jean Viegas <jeanmelg@gmail.com>
2022-10-20 15:04:32 -0300

    feat(file_management): :sparkles: implement txt_importer with errors if path_file is invalid

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('txt'):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
